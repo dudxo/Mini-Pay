1c0a9daf2e7ce88f102ce808ae5e45f8f2d12199
dudxo <father8475@naver.com>
2024-06-10 16:21:36 +0900

    feat: BaseEntity 작성

---
+++ src/main/java/global/entity/BaseEntity.java
@@ -0,0 +1,31 @@
+package global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @Column(name = "create_dt", nullable = false, updatable = false)
+    private LocalDateTime created;
+
+    @Column(name = "create_dt", nullable = false, updatable = false)
+    private LocalDateTime updated;
+
+    @PrePersist
+    protected void onCreate() {
+        created = LocalDateTime.now();
+        onUpdate();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated = LocalDateTime.now();
+    }
+}
