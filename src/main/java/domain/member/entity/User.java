3dfa383192eaf3af770cd215412012b315c13b6e
dudxo <father8475@naver.com>
2024-06-10 16:24:02 +0900

    feat: User Entity 작성

---
+++ src/main/java/domain/member/entity/User.java
@@ -0,0 +1,37 @@
+package domain.member.entity;
+
+import global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "users_id", nullable = false)
+    private Long usersId;
+
+    @Column(name = "name", nullable = false )
+    private String name;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "password", nullable = false)
+    private String email;
+
+    @Builder
+    public User(String name, String password, String email){
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+}
